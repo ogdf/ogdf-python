9550c447696fc15de87f63a26747711d4723a4cc
Andreas Strobl <andreas.strobl123@googlemail.com>
2021-10-07 12:59:35 +0200

    Started work on using ipykernel.comm for Communication between front and back end. Currently, not displaying the Graph when using the widget. Added requirements.txt.

--- js/lib/example.js
+++ js/lib/example.js
@@ -42,12 +42,47 @@ var HelloModel = widgets.DOMWidgetModel.extend({
 var HelloView = widgets.DOMWidgetView.extend({
     // Defines how the widget gets rendered into the DOM
     render: function () {
+        let first_msg = true
+        var nodes
+        var links
+        var self = this
+
         this.svg = document.createElementNS("http://www.w3.org/2000/svg", "svg")
 
         this.svg.setAttribute("width", "960");
         this.svg.setAttribute("height", "540");
 
-        var svg = d3.select(this.svg),
+        // this.el.appendChild(this.svg)
+
+
+        var comm_manager = Jupyter.notebook.kernel.comm_manager
+        var handle_msg = function (msg) {
+            //use msg headers
+            if (first_msg) {
+                first_msg = false
+                nodes = JSON.parse(msg.content.data)
+            } else {
+                links = JSON.parse(msg.content.data)
+                self.draw_graph.call(self, nodes, links, self.svg)
+                self.el.appendChild(self.svg)
+            }
+        }
+
+        comm_manager.register_target('myTarget', function (comm, msg) {
+            comm.on_msg(handle_msg)
+        })
+
+        // this.model.on('change:value', this.value_changed, this);
+    },
+
+    draw_graph(nodes_data, links_data, bild) {
+
+        console.log(nodes_data)
+        console.log(links_data)
+        console.log("drawing graph")
+
+
+        var svg = d3.select(bild),
             width = +svg.attr("width"),
             height = +svg.attr("height");
 
@@ -67,300 +102,6 @@ var HelloView = widgets.DOMWidgetView.extend({
             .attr("fill", "green")
             .append("svg:path")
             .attr("d", "M0,-5L10,0L0,5");
-
-        var nodes_data = [{"id": "0", "name": "0", "x": 250, "y": 40}, {
-            "id": "1",
-            "name": "1",
-            "x": 220,
-            "y": 90
-        }, {"id": "2", "name": "2", "x": 200, "y": 575}, {"id": "3", "name": "3", "x": 150, "y": 732}, {
-            "id": "7",
-            "name": "7",
-            "x": 160,
-            "y": 515
-        }, {"id": "8", "name": "8", "x": 240, "y": 465}, {"id": "9", "name": "9", "x": 40, "y": 575}, {
-            "id": "10",
-            "name": "10",
-            "x": 150,
-            "y": 782
-        }, {"id": "11", "name": "11", "x": 220, "y": 362}, {"id": "12", "name": "12", "x": 380, "y": 262}, {
-            "id": "13",
-            "name": "13",
-            "x": 240,
-            "y": 942
-        }, {"id": "14", "name": "14", "x": 80, "y": 782}, {"id": "15", "name": "15", "x": 290, "y": 262}, {
-            "id": "16",
-            "name": "16",
-            "x": 180,
-            "y": 140
-        }, {"id": "17", "name": "17", "x": 335, "y": 212}, {"id": "18", "name": "18", "x": 260, "y": 312}, {
-            "id": "19",
-            "name": "19",
-            "x": 260,
-            "y": 415
-        }, {"id": "20", "name": "20", "x": 280, "y": 625}, {"id": "21", "name": "21", "x": 240, "y": 855}, {
-            "id": "22",
-            "name": "22",
-            "x": 180,
-            "y": 625
-        }]
-        var links_data = [{"source": "16", "target": "0"}, {
-            "id": "0_1",
-            "sx": 250,
-            "sy": 40,
-            "tx": 220,
-            "ty": 90,
-            "arrow": true
-        }, {"id": "12_20", "sx": 380, "sy": 262, "tx": 360, "ty": 312}, {
-            "id": "12_20",
-            "sx": 360,
-            "sy": 312,
-            "tx": 360,
-            "ty": 575
-        }, {"id": "12_20", "sx": 360, "sy": 575, "tx": 280, "ty": 625, "arrow": true}, {
-            "id": "12_21",
-            "sx": 380,
-            "sy": 262,
-            "tx": 400,
-            "ty": 312
-        }, {"id": "12_21", "sx": 400, "sy": 312, "tx": 400, "ty": 782}, {
-            "id": "12_21",
-            "sx": 400,
-            "sy": 782,
-            "tx": 240,
-            "ty": 855,
-            "arrow": true
-        }, {"id": "11_8", "sx": 220, "sy": 362, "tx": 220, "ty": 415}, {
-            "id": "11_8",
-            "sx": 220,
-            "sy": 415,
-            "tx": 240,
-            "ty": 465,
-            "arrow": true
-        }, {"id": "16_9", "sx": 180, "sy": 140, "tx": 40, "ty": 212}, {
-            "id": "16_9",
-            "sx": 40,
-            "sy": 212,
-            "tx": 40,
-            "ty": 515
-        }, {"id": "16_9", "sx": 40, "sy": 515, "tx": 40, "ty": 575, "arrow": true}, {
-            "id": "2_22",
-            "sx": 200,
-            "sy": 575,
-            "tx": 180,
-            "ty": 625,
-            "arrow": true
-        }, {"id": "14_7", "sx": 80, "sy": 782, "tx": 80, "ty": 732}, {
-            "id": "14_7",
-            "sx": 80,
-            "sy": 732,
-            "tx": 80,
-            "ty": 575
-        }, {"id": "14_7", "sx": 80, "sy": 575, "tx": 160, "ty": 515, "arrow": true}, {
-            "id": "7_2",
-            "sx": 160,
-            "sy": 515,
-            "tx": 200,
-            "ty": 575,
-            "arrow": true
-        }, {"id": "2_20", "sx": 200, "sy": 575, "tx": 280, "ty": 625, "arrow": true}, {
-            "id": "7_9",
-            "sx": 160,
-            "sy": 515,
-            "tx": 40,
-            "ty": 575,
-            "arrow": true
-        }, {"id": "9_14", "sx": 40, "sy": 575, "tx": 40, "ty": 625}, {
-            "id": "9_14",
-            "sx": 40,
-            "sy": 625,
-            "tx": 40,
-            "ty": 732
-        }, {"id": "9_14", "sx": 40, "sy": 732, "tx": 80, "ty": 782, "arrow": true}, {
-            "id": "3_10",
-            "sx": 150,
-            "sy": 732,
-            "tx": 150,
-            "ty": 782,
-            "arrow": true
-        }, {"id": "16_11", "sx": 180, "sy": 140, "tx": 200, "ty": 212}, {
-            "id": "16_11",
-            "sx": 200,
-            "sy": 212,
-            "tx": 200,
-            "ty": 312
-        }, {"id": "16_11", "sx": 200, "sy": 312, "tx": 220, "ty": 362, "arrow": true}, {
-            "id": "19_18",
-            "sx": 260,
-            "sy": 415,
-            "tx": 280,
-            "ty": 362
-        }, {"id": "19_18", "sx": 280, "sy": 362, "tx": 260, "ty": 312, "arrow": true}, {
-            "id": "17_12",
-            "sx": 335,
-            "sy": 212,
-            "tx": 380,
-            "ty": 262,
-            "arrow": true
-        }, {"id": "10_13", "sx": 150, "sy": 782, "tx": 150, "ty": 855}, {
-            "id": "10_13",
-            "sx": 150,
-            "sy": 855,
-            "tx": 240,
-            "ty": 942,
-            "arrow": true
-        }, {"id": "22_3", "sx": 180, "sy": 625, "tx": 150, "ty": 732, "arrow": true}, {
-            "id": "3_14",
-            "sx": 150,
-            "sy": 732,
-            "tx": 80,
-            "ty": 782,
-            "arrow": true
-        }, {"id": "0_15", "sx": 250, "sy": 40, "tx": 290, "ty": 90}, {
-            "id": "0_15",
-            "sx": 290,
-            "sy": 90,
-            "tx": 290,
-            "ty": 212
-        }, {"id": "0_15", "sx": 290, "sy": 212, "tx": 290, "ty": 262, "arrow": true}, {
-            "id": "18_1",
-            "sx": 260,
-            "sy": 312,
-            "tx": 240,
-            "ty": 262
-        }, {"id": "18_1", "sx": 240, "sy": 262, "tx": 240, "ty": 140}, {
-            "id": "18_1",
-            "sx": 240,
-            "sy": 140,
-            "tx": 220,
-            "ty": 90,
-            "arrow": true
-        }, {"id": "1_16", "sx": 220, "sy": 90, "tx": 180, "ty": 140, "arrow": true}, {
-            "id": "0_17",
-            "sx": 250,
-            "sy": 40,
-            "tx": 335,
-            "ty": 90
-        }, {"id": "0_17", "sx": 335, "sy": 90, "tx": 335, "ty": 140}, {
-            "id": "0_17",
-            "sx": 335,
-            "sy": 140,
-            "tx": 335,
-            "ty": 212,
-            "arrow": true
-        }, {"id": "17_15", "sx": 335, "sy": 212, "tx": 290, "ty": 262, "arrow": true}, {
-            "id": "15_18",
-            "sx": 290,
-            "sy": 262,
-            "tx": 260,
-            "ty": 312,
-            "arrow": true
-        }, {"id": "11_19", "sx": 220, "sy": 362, "tx": 260, "ty": 415, "arrow": true}, {
-            "id": "19_8",
-            "sx": 260,
-            "sy": 415,
-            "tx": 240,
-            "ty": 465,
-            "arrow": true
-        }, {"id": "8_20", "sx": 240, "sy": 465, "tx": 240, "ty": 515}, {
-            "id": "8_20",
-            "sx": 240,
-            "sy": 515,
-            "tx": 240,
-            "ty": 575
-        }, {"id": "8_20", "sx": 240, "sy": 575, "tx": 280, "ty": 625, "arrow": true}, {
-            "id": "10_21",
-            "sx": 150,
-            "sy": 782,
-            "tx": 240,
-            "ty": 855,
-            "arrow": true
-        }, {"id": "21_13", "sx": 240, "sy": 855, "tx": 240, "ty": 942, "arrow": true}, {
-            "id": "13_22",
-            "sx": 240,
-            "sy": 942,
-            "tx": 440,
-            "ty": 855
-        }, {"id": "13_22", "sx": 440, "sy": 855, "tx": 440, "ty": 732}, {
-            "id": "13_22",
-            "sx": 440,
-            "sy": 732,
-            "tx": 180,
-            "ty": 625,
-            "arrow": true
-        }, {"id": "11_7", "sx": 220, "sy": 362, "tx": 120, "ty": 415}, {
-            "id": "11_7",
-            "sx": 120,
-            "sy": 415,
-            "tx": 120,
-            "ty": 465
-        }, {"id": "11_7", "sx": 120, "sy": 465, "tx": 160, "ty": 515, "arrow": true}, {
-            "id": "16_17",
-            "sx": 180,
-            "sy": 140,
-            "tx": 335,
-            "ty": 212,
-            "arrow": true
-        }, {"id": "22_7", "sx": 180, "sy": 625, "tx": 160, "ty": 575}, {
-            "id": "22_7",
-            "sx": 160,
-            "sy": 575,
-            "tx": 160,
-            "ty": 515,
-            "arrow": true
-        }, {"id": "8_7", "sx": 240, "sy": 465, "tx": 160, "ty": 515, "arrow": true}, {
-            "id": "3_7",
-            "sx": 150,
-            "sy": 732,
-            "tx": 120,
-            "ty": 625
-        }, {"id": "3_7", "sx": 120, "sy": 625, "tx": 120, "ty": 575}, {
-            "id": "3_7",
-            "sx": 120,
-            "sy": 575,
-            "tx": 160,
-            "ty": 515,
-            "arrow": true
-        }, {"id": "21_22", "sx": 240, "sy": 855, "tx": 240, "ty": 782}, {
-            "id": "21_22",
-            "sx": 240,
-            "sy": 782,
-            "tx": 240,
-            "ty": 732
-        }, {"id": "21_22", "sx": 240, "sy": 732, "tx": 180, "ty": 625, "arrow": true}, {
-            "id": "7_16",
-            "sx": 160,
-            "sy": 515,
-            "tx": 160,
-            "ty": 465
-        }, {"id": "7_16", "sx": 160, "sy": 465, "tx": 160, "ty": 212}, {
-            "id": "7_16",
-            "sx": 160,
-            "sy": 212,
-            "tx": 180,
-            "ty": 140,
-            "arrow": true
-        }, {"id": "18_11", "sx": 260, "sy": 312, "tx": 220, "ty": 362, "arrow": true}, {
-            "id": "20_15",
-            "sx": 280,
-            "sy": 625,
-            "tx": 320,
-            "ty": 575
-        }, {"id": "20_15", "sx": 320, "sy": 575, "tx": 320, "ty": 312}, {
-            "id": "20_15",
-            "sx": 320,
-            "sy": 312,
-            "tx": 290,
-            "ty": 262,
-            "arrow": true
-        }, {"id": "16_0", "sx": 180, "sy": 140, "tx": 180, "ty": 90}, {
-            "id": "16_0",
-            "sx": 180,
-            "sy": 90,
-            "tx": 250,
-            "ty": 40,
-            "arrow": true
-        }]
 
         //add encompassing group for the zoom
         var g = svg.append("g").attr("class", "everything");
@@ -410,9 +151,16 @@ var HelloView = widgets.DOMWidgetView.extend({
             })
             .attr("r", radius)
             .attr("fill", "green")
+            .attr("id", function (d) {
+                return d.id
+            })
             .on("click", function (event) {
                 console.log("on click " + event.target.__data__.name);
-            });
+            })
+            .on("mouseover", handleMouseOver)
+            .on("mouseout", handleMouseOut);
+
+        console.log(nodes)
 
         var text = g.append("g")
             .attr("class", "texts")
@@ -423,6 +171,9 @@ var HelloView = widgets.DOMWidgetView.extend({
             .attr("text-anchor", "middle")
             .attr("dominant-baseline", "central")
             .attr("fill", "white")
+            .attr("id", function (d) {
+                return d.id
+            })
             .text(function (d) {
                 return d.name;
             })
@@ -439,14 +190,25 @@ var HelloView = widgets.DOMWidgetView.extend({
             .extent([[0, 0], [width, height]])
             .on("zoom", zoomed));
 
+
+        // this.el.appendChild(this.svg);
+
+
         function zoomed({transform}) {
             g.attr("transform", transform);
         }
 
-        this.el.appendChild(this.svg);
+        // Create Event Handlers for mouse
+        function handleMouseOver(d, i) {  // Add interactivity
+            // Use D3 to select element, change color and size
+            d3.select(this).attr("fill", "orange").attr("r", radius * 1.5);
+        }
 
-        // this.model.on('change:value', this.value_changed, this);
-    },
+        function handleMouseOut(d, i) {
+            // Use D3 to select element, change color back to normal
+            d3.select(this).attr("fill", "green").attr("r", radius);
+        }
+    }
 });
 
 
--- ogdf_python_widget/widget.py
+++ ogdf_python_widget/widget.py
@@ -1,58 +1,50 @@
-from ipywidgets import HBox, VBox, Button
+import json
+
+from ipywidgets import VBox, Button
 from .example import HelloWorld
+from ipykernel.comm import Comm
 
 
 class GraphWidget(VBox):
     def __init__(self, graph_attributes):
+
+        self.c = Comm(target_name='myTarget', data={})
+
         self.graph_attributes = graph_attributes
         self.drawing_pad = HelloWorld()
-        self.drawing_pad.on_click(lambda b: print('test'))
-        add_button = Button(description="Add", tooltip="Click me")
-        clear_button = Button(description="Clear", tooltip="Click me")
-        refresh_button = Button(description="Refresh", tooltip="Click me")
         load_button = Button(description="Load Graph", tooltip="Click me")
-        add_button.on_click(lambda b: self.test())
-        clear_button.on_click(lambda b: self.clear())
-        refresh_button.on_click(lambda b: self.refresh())
         load_button.on_click(lambda b: self.export_graph())
-        self.count = 0
-        self.test()
         self.export_graph()
 
-        buttons = VBox([self.drawing_pad, add_button, clear_button, refresh_button, load_button])
+        buttons = VBox([self.drawing_pad, load_button])
         super().__init__([buttons])
 
     def export_graph(self):
-        nodes = []
-        edges = []
-
+        nodes_data = []
         for node in self.graph_attributes.constGraph().nodes:
-            x_coord = int(self.graph_attributes.x(node) + 0.5)
-            y_coord = int(self.graph_attributes.y(node) + 0.5)
-            nodes.append([x_coord, y_coord, str(node.index())])
+            nodes_data.append(
+                {"id": str(node.index()), "name": str(node.index()), "x": int(self.graph_attributes.x(node) + 0.5),
+                 "y": int(self.graph_attributes.y(node) + 0.5)})
+
+        links_data = []
 
         for edge in self.graph_attributes.constGraph().edges:
+            edge_id = str(edge.source().index()) + "_" + str(edge.target().index())
             prev_x = int(self.graph_attributes.x(edge.source()) + 0.5)
             prev_y = int(self.graph_attributes.y(edge.source()) + 0.5)
 
             for point in self.graph_attributes.bends(edge):
-                edges.append([prev_x, prev_y, int(point.m_x + 0.5), int(point.m_y + 0.5)])
+                links_data.append(
+                    {"id": edge_id, "sx": prev_x, "sy": prev_y, "tx": int(point.m_x + 0.5), "ty": int(point.m_y + 0.5)})
                 prev_x = int(point.m_x + 0.5)
                 prev_y = int(point.m_y + 0.5)
 
-            edges.append([prev_x, prev_y, int(self.graph_attributes.x(edge.target()) + 0.5),
-                          int(self.graph_attributes.y(edge.target()) + 0.5)])
+            link_dict = {"id": edge_id, "sx": prev_x, "sy": prev_y,
+                         "tx": int(self.graph_attributes.x(edge.target()) + 0.5),
+                         "ty": int(self.graph_attributes.y(edge.target()) + 0.5)}
+            if self.graph_attributes.arrowType(edge) == 1:
+                link_dict['arrow'] = True
+            links_data.append(link_dict)
 
-        self.drawing_pad.set_trait('nodes', nodes)
-        self.drawing_pad.set_trait('edges', edges)
-
-    def test(self):
-        self.count += 1
-        self.drawing_pad.value = self.count.__str__()
-
-    def clear(self):
-        self.count = 0
-        self.drawing_pad.value = self.count.__str__()
-
-    def refresh(self):
-        self.drawing_pad.refresh = not self.drawing_pad.refresh
+        self.c.send(json.dumps(nodes_data))
+        self.c.send(json.dumps(links_data))
