e6690a27dd6eedf79f53258af4b0212a92fbebba
Simon Dominik Niko Fink <simon.fink@uni-passau.de>
2023-09-19 12:16:06 +0200

    switch to repr_mimebundle

--- src/ogdf_python/matplotlib/__init__.py
+++ src/ogdf_python/matplotlib/__init__.py
@@ -11,30 +11,36 @@ with as_file(files(__package__).joinpath("rendering.h")) as header:
     cppinclude(header)
 
 
-# cppinclude("Python.h")
+def repr_mimebundle_GraphAttributes(self, *args, **kwargs):
+    from IPython.core.interactiveshell import InteractiveShell
 
+    if not InteractiveShell.initialized():
+        return {"text/plain": repr(self)}, {}
 
-def display_GraphAttributes(self):
-    from IPython.display import display
     from ogdf_python.matplotlib.widget import MatplotlibGraph
     fig = new_figure()
     ax = fig.subplots()
     fig.graph = MatplotlibGraph(self, ax)
-    display(fig.canvas)
-    return None
+    data, meta = InteractiveShell.instance().display_formatter.format(fig.canvas, *args, **kwargs)
+    if not data:
+        # object handled itself through display, don't proceed
+        return data, meta
+    else:
+        data["text/plain"] = repr(self)
+        return data, meta
 
 
-def display_Graph(self):
+def repr_mimebundle_Graph(self, *args, **kwargs):
     from ogdf_python.pythonize import renderGraph
-    return display_GraphAttributes(renderGraph(self))
+    return repr_mimebundle_GraphAttributes(renderGraph(self), *args, **kwargs)
 
 
-def display_ClusterGraph(self):
+def repr_mimebundle_ClusterGraph(self, *args, **kwargs):
     from ogdf_python.pythonize import renderClusterGraph
-    return display_GraphAttributes(renderClusterGraph(self))
+    return repr_mimebundle_GraphAttributes(renderClusterGraph(self), *args, **kwargs)
 
 
-ogdf.GraphAttributes._ipython_display_ = display_GraphAttributes
-ogdf.Graph._ipython_display_ = display_Graph
-ogdf.ClusterGraphAttributes._ipython_display_ = display_GraphAttributes
-ogdf.ClusterGraph._ipython_display_ = display_ClusterGraph
+ogdf.GraphAttributes._repr_mimebundle_ = repr_mimebundle_GraphAttributes
+ogdf.Graph._repr_mimebundle_ = repr_mimebundle_Graph
+ogdf.ClusterGraphAttributes._repr_mimebundle_ = repr_mimebundle_GraphAttributes
+ogdf.ClusterGraph._repr_mimebundle_ = repr_mimebundle_ClusterGraph
