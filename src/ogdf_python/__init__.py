2a62b88ecd923a6b3284872d65f745d3f081c146
Simon D Fink <finksim@fim.uni-passau.de>
2024-07-18 16:02:58 +0200

    Bump version: 0.3.2 → 0.3.3-dev

--- src/ogdf_python/__init__.py
+++ src/ogdf_python/__init__.py
@@ -15,7 +15,7 @@ __keep_imports = [
     ogdf_python.jupyter,
 ]
 
-__version__ = "0.3.2"
+__version__ = "0.3.3-dev"
 __all__ = ogdf_python.loader.__all__ + ogdf_python.utils.__all__ + ogdf_python.info.__all__
 
 try:
