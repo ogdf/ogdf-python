a2231ca396c76ad2cfbff79a6ddd9acca478ebf4
Simon D Fink <finksim@fim.uni-passau.de>
2026-03-13 11:32:24 +0100

    update zip script

--- docs/build-tutorial-zip.sh
+++ docs/build-tutorial-zip.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-rm tutorial.zip
+set -e
+
+if ! command -v jupytext &> /dev/null; then
+    echo "Error: jupytext not found." >&2
+    exit 1
+fi
+
+rm -f tutorial.zip
 latexmk tutorial
 
 for f in {examples,exercises,tutorial}/*.py; do
 	jupytext -o "${f%.*}.ipynb" "$f"
 done
 
-zip -r tutorial.zip data examples/*.ipynb exercises/*.ipynb tutorial/*.ipynb build-ogdf.sh tutorial.pdf
+zip -r tutorial.zip data examples/*.ipynb exercises/*.ipynb tutorial/*.ipynb tutorial/img build-ogdf.sh tutorial.pdf
 
-rm {examples,exercises,tutorial}/*.ipynb
+rm -f {examples,exercises,tutorial}/*.ipynb
